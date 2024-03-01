6335a96a83eb757e59ab7a240050392cdb5abf63
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-02-29 21:38:27 -0300

    JWT

---
+++ middlewares/userValidations.js
@@ -0,0 +1,27 @@
+const { body } = require("express-validator")
+
+const userCreateValidation = () => {
+    return [
+        body("name")
+            .isString().withMessage("O nome é obrigatório.")
+            .isLength({ min: 3 }).withMessage("O nome deve conter no mínimo 3 caracteres."),
+        body("email")
+            .isString().withMessage("O e-mail é obrigatório.")
+            .isEmail().withMessage("Informe um e-mail válido."),
+        body("password")
+            .isString().withMessage("A senha é obrigatória.")
+            .isLength({ min: 5 }).withMessage("A senha deve conter no mínimo 5 caracteres."),
+        body("confirmpassword")
+            .isString().withMessage("A confirmação de senha é obrigatória.")
+            .custom((value, { req }) => {
+                if (value != req.body.password) {
+                    throw new Error("As senhas são diferentes.")
+                }
+                return true
+            }),
+    ]
+}
+
+module.exports = {
+    userCreateValidation,
+}
