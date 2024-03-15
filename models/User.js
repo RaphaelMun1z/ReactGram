d4499f35da6611d0f9ec836969722bfaf99fb843
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-03-14 21:47:22 -0300

    Atualização

--- models/User.js
+++ models/User.js
@@ -8,6 +8,7 @@ const userSchema = new Schema(
         password: String,
         profileImage: String,
         bio: String,
+        privateProfile: { type: Boolean, default: true },
         following: Array,
         followers: Array,
     },
